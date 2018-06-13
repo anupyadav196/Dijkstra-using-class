5f57a261bf125cf69e19705b4f4d94287485c79e
anupyadav196 <35986782+anupyadav196@users.noreply.github.com>
2018-06-13 07:17:03 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,50 +1,9 @@
 package com.hackercode.Driver;
 import com.hackercode.utility.*;
-
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Hashtable;
-
-import java.util.Vector;
 public class Driver {
 
     public static void main(String[] args) {
-  /*      int a,b;
-        Scanner in = new Scanner(System.in);
-        a = in.nextInt();
-        b = in.nextInt();
-        System.out.println("addition is " + Operation.add(a, b));
-        System.out.println("subtraction is " + Operation.sub(a, b));
-        System.out.println("multiplication is " + Operation.mul(a, b));
-        System.out.println("division is " + Operation.div(a, b));
-        int arr[] = {34, 35, 67, 23, 45, 46};
-        Selectionsort obj = new Selectionsort();
-        obj.sort(arr);
-*/
-
-        //array list exam
-     /*
-        int n = 10;
-        ArrayList<Integer> arrli = new ArrayList<Integer>(10);
-        for (int i = 0; i < n; i++) {
-            arrli.add(i);
-        }
-        System.out.println(arrli);
-        for (int i = 0; i < n; i++) {
-            System.out.println(arrli.get(i) + "");
-        }
-        int arr []= new int[10];
-        Vector<Integer> v = new Vector();
-        Hashtable<Integer , String > h= new Hashtable
-        arr[0]=10;
-        Integer obj;
-        v.addElement( obj: 10);
-        h.put(12,"Hackercode");
-        arrli.add(10);
-*/
-
-
         int n = 5;
         int inf = 999;
         //int src;
